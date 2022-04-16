719f8a162882dc7f458fa4e8341e3cbf6bf00215
pantinga <pantinga@mail.ru>
2022-04-16 20:55:55 +0300

    module3-task1

--- source/js/menu.js
+++ source/js/menu.js
@@ -4,15 +4,15 @@ var ulMenu = document.querySelector('.main-navigation__list-none');
 var mainPage = document.querySelector('.page-main');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('toggle-open')) {
-    navMain.classList.remove('toggle-open');
-    navMain.classList.add('toggle-close');
+  if (navMain.classList.contains('toggle__open')) {
+    navMain.classList.remove('toggle__open');
+    navMain.classList.add('toggle__close');
     ulMenu.classList.remove('main-navigation__list-none');
     mainPage.classList.add('page-main__height');
 
     } else {
-      navMain.classList.add('toggle-open');
-      navMain.classList.remove('toggle-close');
+      navMain.classList.add('toggle__open');
+      navMain.classList.remove('toggle__close');
       ulMenu.classList.add('main-navigation__list-none');
       mainPage.classList.remove('page-main__height');
     }
