580106aaf5da86171fd98866598c5302a45adc89
pantinga <pantinga@mail.ru>
2022-05-11 10:52:25 +0300

    master

--- source/js/menu1.js
+++ source/js/menu1.js
@@ -1,25 +1,22 @@
 var pageHead = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__toggle');
-var ulMenu = document.querySelector('.main-navigation__list-none');
-var pageHenojs = document.querySelector('.page-header--nojs');
-var pageHetonojs = document.querySelector('.page-header__toggle-nojs');
-var mainNavnojs = document.querySelector('.main-navigation__list-nojs');
-var foodNojs = document.querySelector('.food--nojs');
+var ulMenu = document.querySelector('.main-navigation__list');
+var food = document.querySelector('.food');
 
-pageHenojs.classList.remove('page-header--nojs');
-pageHetonojs.classList.remove('page-header__toggle-nojs');
-mainNavnojs.classList.remove('main-navigation__list-nojs');
-foodNojs.classList.remove('food--nojs');
+pageHead.classList.remove('page-header--nojs');
+navToggle.classList.remove('page-header__toggle--nojs');
+ulMenu.classList.remove('main-navigation__list--nojs');
+food.classList.remove('food--nojs');
 
 navToggle.addEventListener('click', function() {
   if (pageHead.classList.contains('page-header--open')) {
     pageHead.classList.remove('page-header--open');
     pageHead.classList.add('page-header--close');
-    ulMenu.classList.remove('main-navigation__list-none');
+    ulMenu.classList.remove('main-navigation__list--none');
 
     } else {
       pageHead.classList.add('page-header--open');
       pageHead.classList.remove('page-header--close');
-      ulMenu.classList.add('main-navigation__list-none');
+      ulMenu.classList.add('main-navigation__list--none');
     }
 });
--- source/js/menu2.js
+++ source/js/menu2.js
@@ -1,23 +1,20 @@
 var pageHead = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__toggle');
-var ulMenu = document.querySelector('.main-navigation__list-none');
-var pageHenojs = document.querySelector('.page-header--nojs');
-var pageHetonojs = document.querySelector('.page-header__toggle-nojs');
-var mainNavnojs = document.querySelector('.main-navigation__list-nojs');
+var ulMenu = document.querySelector('.main-navigation__list');
 
-pageHenojs.classList.remove('page-header--nojs');
-pageHetonojs.classList.remove('page-header__toggle-nojs');
-mainNavnojs.classList.remove('main-navigation__list-nojs');
+pageHead.classList.remove('page-header--nojs');
+navToggle.classList.remove('page-header__toggle--nojs');
+ulMenu.classList.remove('main-navigation__list--nojs');
 
 navToggle.addEventListener('click', function() {
   if (pageHead.classList.contains('page-header--open')) {
     pageHead.classList.remove('page-header--open');
     pageHead.classList.add('page-header--close');
-    ulMenu.classList.remove('main-navigation__list-none');
+    ulMenu.classList.remove('main-navigation__list--none');
 
     } else {
       pageHead.classList.add('page-header--open');
       pageHead.classList.remove('page-header--close');
-      ulMenu.classList.add('main-navigation__list-none');
+      ulMenu.classList.add('main-navigation__list--none');
     }
 });
