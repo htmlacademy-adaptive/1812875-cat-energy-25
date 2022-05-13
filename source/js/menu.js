4967c6a74d2f50b852d3a67316b0deb0686b890d
pantinga <pantinga@mail.ru>
2022-05-13 11:28:24 +0300

    master123123123

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,10 +1,14 @@
 var pageHead = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__toggle');
 var ulMenu = document.querySelector('.main-navigation__list');
+var food = document.querySelector('.food');
 
 pageHead.classList.remove('page-header--nojs');
 navToggle.classList.remove('page-header__toggle--nojs');
 ulMenu.classList.remove('main-navigation__list--nojs');
+if (food) {
+  food.classList.remove('food--nojs');
+}
 
 navToggle.addEventListener('click', function() {
   if (pageHead.classList.contains('page-header--open')) {
